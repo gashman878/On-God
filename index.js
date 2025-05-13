b9bd8940445fcf4d722036c9cc63657d39b87402
gashman878 <gashman878@gmail.com>
2025-05-13 01:36:15 +0100

    thanks

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ function switchMode(event) {
     const stylesheet = document.getElementById("themeStylesheet");
     stylesheet.href = "whiteMode.css";
     event.preventDefault()
+    let buttonClass = document.querySelector('.last_link');
+
+    console.log(buttonClass.innerHTML);
+    if (buttonClass.innerHTML === 'light mode') {
+        buttonClass.innerHTML = 'dark mode';
+    } else {
+        buttonClass.innerHTML = 'light mode';
+        stylesheet.href = "index-desktop.css";
+    }
+    
 }
